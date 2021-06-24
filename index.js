36962fd67eda564db916809311e0262cbbd8f56d
savanna chiang <savannaac@gmail.com>
2021-06-24 01:44:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const newArray = []
+    // let event = birthday
+
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--)
+    }
+}
